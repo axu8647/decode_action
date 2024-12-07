971f61a42c11945a71ef64c1804687cb4c5c6e42
action <action@github.com>
2024-12-07 18:41:38 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sat Dec 07 2024 18:31:13 GMT+0000 (Coordinated Universal Time)
+//Sat Dec 07 2024 18:41:38 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 console.log("=>=>=>=>____来自 By 幼稚园小妹妹 (顶级插件售后服务951584089)丨Autman订阅源:Lxg-021002丨期待为您服务<=<=<=<=");
